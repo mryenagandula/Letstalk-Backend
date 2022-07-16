b5aea088923f781b6e910ca02e0d903b738f2090
SpringSrikanth <srikanthyenagagandula89@gmail.com>
2022-07-16 20:20:22 +0530

    Initial Commit

---
+++ src/routes/audit.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const requireAuth = require('../middlewares/requireauth.js');
+
+const adminauth = require('../middlewares/adminauth');
+
+const mongoose = require('mongoose');
+
+const Audit = mongoose.model('Audit');
+
+const router = express.Router();
+
+router.get('/audits',[requireAuth,adminauth],async (req, res)=>{
+    try {
+        const audits = await Audit.find({});
+        res.status(200).send({audits})
+    } catch (error) {
+        console.log(error.message);
+		res.status(500).send({message:error.message});
+    }
+})
+
+router.get('/audits/:email',[requireAuth,adminauth],async (req, res)=>{
+    try {
+        const audits = await Audit.find({email: req.params.email});
+        res.status(200).send({audits})
+    } catch (error) {
+        console.log(error.message);
+		res.status(500).send({message:error.message});
+    }
+})
+
+router.get('/audits/:pageIndex/:pageSize',[requireAuth,adminauth],async(req,res)=>{
+	try {
+		const {pageIndex,pageSize}= req.params;
+		const allAudits = await Audit.find();
+		const audits = await Audit.find({}).limit(parseInt(pageSize)).skip(parseInt(pageSize)*parseInt(pageIndex));
+		res.status(200).json({audits, totalCount: allAudits.length})
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).send({message:error.message});
+	}
+})
+
+router.get('/audits/:email/:pageIndex/:pageSize',[requireAuth,adminauth],async(req,res)=>{
+	try {
+		const {email,pageIndex,pageSize}= req.params;
+		const allAudits = await Audit.find({email});
+		const audits = await Audit.find({email}).limit(parseInt(pageSize)).skip(parseInt(pageSize)*parseInt(pageIndex));
+		res.status(200).json({audits, totalCount: allAudits.length})
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).send({message:error.message});
+	}
+})
+
+module.exports =router;
---
+++ src/routes/public.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Blog = mongoose.model('Blog');
+const Audit = mongoose.model('Audit');
+
+const router = express.Router();
+
+router.get('/public/blogs', async (req, res) => {
+    try {
+        const blogs = await Blog.find();
+        res.status(201).send({blogs,totalCount:blogs.length});
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send({
+            message: err.message
+        })
+    }
+})
+
+router.get('/public/blogs/:pageIndex/:pageSize/:published', async (req, res) => {
+    try {
+        const {pageIndex,pageSize,published}= req.params;
+		const allBlogs = await Blog.find({published});
+		const blogs = await Blog.find({published}).limit(parseInt(pageSize)).skip(parseInt(pageSize)*parseInt(pageIndex));
+        res.status(201).send({blogs,totalCount:allBlogs.length});
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send({
+            message: err.message
+        })
+    }
+})
+
+router.get('/public/audits/:pageIndex/:pageSize',async(req,res)=>{
+    try {
+        const filter= {}
+        const queryParams = req?.query;
+        if(req?.query){
+            if(queryParams.email){
+                filter.email = queryParams.email
+            }
+            if(queryParams.statusCode){
+                filter.statusCode = queryParams.statusCode
+            }
+        }
+		const {pageIndex,pageSize}= req.params;
+		const allAudits = await Audit.find(filter);
+		const audits = await Audit.find(filter).limit(parseInt(pageSize)).skip(parseInt(pageSize)*parseInt(pageIndex));
+		res.status(200).json({audits, totalCount: allAudits.length})
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).send({message:error.message});
+	}
+})
+
+router.get('/public/audits/filter-values',async(req,res)=>{
+    try {
+		const  statusCodes= await Audit.find({},{ statusCode:1,_id:0 })
+        const  statusMessages= await Audit.find({},{ statusMessage: 1,_id:0 })
+        const  emails= await Audit.find({},{ email:1,_id:0 })
+		res.status(200).json({
+            statusCodes: [...new Set(statusCodes.map(item=> item.statusCode))],
+            statusMessages: [...new Set(statusMessages.map(item=> item.statusMessage))],
+            emails: [...new Set(emails.map(item=> item.email))],
+        })
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).send({message:error.message});
+	}
+})
+
+module.exports = router;
